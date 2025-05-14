da8dbe07802134c7e150453b86a02b4680ea5744
Alex-S-3 <aks9106@nyu.edu>
2025-05-14 14:53:39 -0400

    Update matcher.py

--- models/matcher.py
+++ models/matcher.py
@@ -75,12 +75,8 @@ class HungarianMatcher(nn.Module):
         # Final cost matrix
         C = self.cost_bbox * cost_bbox + self.cost_class * cost_class + self.cost_giou * cost_giou
         C = C.view(bs, num_queries, -1).cpu()
-        print("!:", targets, targets.shape)
-        sizes = [len(v["boxes"]) for v in targets]
-        print("2:", sizes)
-        print("2.1:", C)
+        sizes = [len(v["boxes"][0]) for v in targets]
         indices = [linear_sum_assignment(c[i]) for i, c in enumerate(C.split(sizes, -1))]
-        print("3:", indices)
         return [(torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64)) for i, j in indices]
 
 
