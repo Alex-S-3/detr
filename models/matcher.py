316f174b96251b66b06d32ac01553f50bf89d43e
Alex-S-3 <aks9106@nyu.edu>
2025-05-14 14:32:31 -0400

    Update matcher.py

--- models/matcher.py
+++ models/matcher.py
@@ -78,7 +78,9 @@ class HungarianMatcher(nn.Module):
         C = C.view(bs, num_queries, -1).cpu()
 
         sizes = [len(v["boxes"]) for v in targets]
+        print("2:", sizes)
         indices = [linear_sum_assignment(c[i]) for i, c in enumerate(C.split(sizes, -1))]
+        print("3:", indices)
         return [(torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64)) for i, j in indices]
 
 
