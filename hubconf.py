9c929646b14b4d9681dca770243981aaaa421d86
Alex-S-3 <aks9106@nyu.edu>
2025-05-14 12:27:07 -0400

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -35,7 +35,8 @@ def detr_resnet50(pretrained=False, num_classes=91, return_postprocessor=False):
     weight_dict = {'loss_ce': 1, 'loss_bbox': 5}
     weight_dict['loss_giou'] = 2
     losses = ['labels', 'boxes', 'cardinality']
-    matcher = build_matcher({'set_cost_class': 10, 'set_cost_bbox': 5, 'set_cost_giou': 2})
+    args = {'set_cost_class': 10, 'set_cost_bbox': 5, 'set_cost_giou': 2}
+    matcher = build_matcher(args)
     if pretrained:
         checkpoint = torch.hub.load_state_dict_from_url(
             url="https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth", map_location="cpu", check_hash=True
